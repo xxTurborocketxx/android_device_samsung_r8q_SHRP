8b75cbfa39d83e1515114734ecaa34d763f22e1f
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2020-11-13 22:11:56 +0200

    r8q: Add TWRP fastbootd support

--- device.mk
+++ device.mk
@@ -14,5 +14,10 @@
 # limitations under the License.
 #
 
-# Partitions
+# Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
