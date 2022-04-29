3f378bdff636a39b94e8f4e6e1cc69835edc6a5c
xxTurborocketxx <tgeywitz8@gmail.com>
2022-04-29 15:05:46 +0200

    vendorsetup.sh: Fix flashlight path

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -45,7 +45,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		export OF_USE_MAGISKBOOT=1
 		export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 		export OF_FLASHLIGHT_ENABLE=1
-		export OF_FL_PATH1="/system/bin/flashlight"
+		export OF_FL_PATH1="/system/flashlight"
 		export OF_FL_PATH2=""
 		export FOX_VERSION="R11.1_1"
 		export OF_SCREEN_H="2400"
