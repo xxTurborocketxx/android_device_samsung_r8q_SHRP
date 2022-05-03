67912b360ee2ca64c24cd2a01fcd2ec073199631
Johx22 <johannjose2004@gmail.com>
2022-05-03 20:56:16 +0300

    Nuke maintainer avatar

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -59,7 +59,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		export OF_STATUS_H="72"
 		export OF_USE_GREEN_LED=0
     		export OF_MAINTAINER="Johx22 & xxTurborocketxx"
-   	 	export OF_MAINTAINER_AVATAR="$PWD/device/samsung/r8q/maintainer.png"
 
 		#R11
 		export FOX_R11=1
